bf4ae67f93c7902b1dc3178f601ebbe43cbc9588
Shobhit Aryan <shobhitaryan2003@gmail.com>
2026-02-02 17:53:00 +0530

    fix: use MONGO_URI env variable

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //connects to database
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT).then(() => {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
 
